5b47b55d5483abba9df2275b844352c001f4ca76
1ds23cs069-dsmk <1ds23cs069@dsce.edu.in>
2026-02-26 22:09:26 +0530

    Removed api folder

--- api/inngest.js
+++
@@ -1,8 +0,0 @@
-import { serve } from "inngest/vercel";
-import { inngest } from "../server/inngest/index.js";
-import { functions } from "../server/inngest/index.js";
-
-export default serve({
-  client: inngest,
-  functions,
-});
