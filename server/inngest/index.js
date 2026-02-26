ebde784894a25a47da7bfda199edd3ab44509db2
1ds23cs069-dsmk <1ds23cs069@dsce.edu.in>
2026-02-26 22:22:11 +0530

    Fix User import and DB connection

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
+import User from "../models/User.js";
+import connectDB from "../configs/db.js";
 
 // Create a client to send and receive events
 
@@ -15,6 +17,7 @@ const syncUserCreation = inngest.createFunction(
   { id: 'sync-user-from-clerk' },
   { event: 'clerk/user.created' },
   async ({ event }) => {
+    await connectDB();
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
     const userData = {
@@ -30,9 +33,11 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest Function to save user data to a database
 const syncUserDeletion = inngest.createFunction(
+    
   { id: 'delete-user-with-clerk' },
   { event: 'clerk/user.deleted' },
   async ({ event }) => {
+    await connectDB();
     const { id } = event.data;
     
     await User.findByIdAndDelete(id);   
