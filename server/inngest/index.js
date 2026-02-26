8ea24b36c50be33bbb3045d90c4a46a8a4326060
1ds23cs069-dsmk <1ds23cs069@dsce.edu.in>
2026-02-26 21:45:19 +0530

    Added Inngest signing key

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,13 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+
+// Create a client to send and receive events
+export const inngest = new Inngest({
+  id: "movie-ticket-booking",
+  signingKey: process.env.INNGEST_SIGNING_KEY,   // 🔥 ADD THIS
+});
 
 
 // Inngest Function to save user data to a database
